5ccd723e5b998e9dbe7c7260a66ea811a2f7b967
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-04-05 19:06:19 -0300

    :sparkles: Creating the healing action: add print_move_message(player, :heal, life)

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -27,4 +27,7 @@ defmodule ExMon.Game.Status do
 
   def print_move_message(:player, :attack, damage), do:
     IO.puts("\n===== The Computer attacked the player causing #{damage} damage. =====\n")
+
+  def print_move_message(player, :heal, life), do:
+    IO.puts("\n===== The #{player} healled itself to #{life} life points. =====\n")
 end
